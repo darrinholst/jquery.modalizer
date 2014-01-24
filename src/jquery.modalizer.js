ba96e0c5d38bfaa989f4968c4bd3144ba3b54c99
Darrin Holst <darrinholst@gmail.com>
2014-01-24 11:08:13 -0600

    single quotes

--- src/jquery.modalizer.js
+++ src/jquery.modalizer.js
@@ -1,13 +1,13 @@
 $.fn.modalize = function() {
-  $donor = $("<div>").html(this.html());
+  $donor = $('<div>').html(this.html());
 
-  $temp = $("<div>");
-  $header = $("<div>").addClass("modal-header").appendTo($temp);
-  $body = $("<div>").addClass("modal-body").appendTo($temp);
-  $footer = $("<div>").addClass("modal-footer").appendTo($temp);
+  $temp = $('<div>');
+  $header = $('<div>').addClass('modal-header').appendTo($temp);
+  $body = $('<div>').addClass('modal-body').appendTo($temp);
+  $footer = $('<div>').addClass('modal-footer').appendTo($temp);
 
-  $donor.find("h2:first").appendTo($header);
-  $donor.find(".actions:first").appendTo($footer);
+  $donor.find('h2:first').appendTo($header);
+  $donor.find('.actions:first').appendTo($footer);
   $body.append($donor.html());
 
   return $temp.html();
